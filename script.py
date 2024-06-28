0300ca53d6fd00d4ff66b16e67ed92a51ab88ebe
harusame1018 <163406621+harusame1018@users.noreply.github.com>
2024-06-28 10:24:00 +0000

    edit script.py

--- script.py
+++ script.py
@@ -9,10 +9,12 @@ class Test:
     def _attack_(attack):
         print(attack,"のダメージを与えた!")
 class monster:
-    def __init__(self,attack,name,hp):
+    def __init__(self,hp,attack,name):
         self.attack = attack;
         self.name = name;
         self.hp = hp;
     def _defens_(defens):
         hp -= player.attack
 player = Test(3,{playername},2,10)
+gobrin = monster(5,5,"gobrin")
+gigarosh = monster()
